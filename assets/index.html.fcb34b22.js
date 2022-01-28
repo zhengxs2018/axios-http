1cbcec0b7aeec5422518ab27974b38ff1e661ec0
zhengxs <zhengxs2018@foxmail.com>
2022-01-28 12:54:53 +0800

    Updates

---
+++ assets/index.html.fcb34b22.js
@@ -0,0 +1,9 @@
+import{c as n}from"./app.5a277da4.js";import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";const s={},e=n(`<h2 id="\u8BA9\u8BF7\u6C42\u66F4\u53EF\u63A7" tabindex="-1"><a class="header-anchor" href="#\u8BA9\u8BF7\u6C42\u66F4\u53EF\u63A7" aria-hidden="true">#</a> \u8BA9\u8BF7\u6C42\u66F4\u53EF\u63A7</h2><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token comment"># With NPM</span>
+$ <span class="token function">npm</span> <span class="token function">install</span> @zhengxs/axios-http --save
+
+<span class="token comment"># With Yarn</span>
+$ <span class="token function">yarn</span> <span class="token function">add</span> @zhengxs/axios-http
+
+<span class="token comment"># With PNPM</span>
+$ <span class="token function">pnpm</span> <span class="token function">add</span> @zhengxs/axios-http
+</code></pre></div>`,2);function t(c,o){return e}var r=a(s,[["render",t]]);export{r as default};
